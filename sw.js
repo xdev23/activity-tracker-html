97c9b701a6c2bcdb5457f73387549e6e3a246ef1
xdev23 <xdevesh1@proton.me>
2025-07-28 22:30:00 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
 // IMPORTANT: If you make any changes to your HTML, CSS, or this file,
 // you MUST increment this version number (e.g., 'v2', 'v3', etc.).
 // This tells the browser to install the new service worker and cache the new files.
-const CACHE_NAME = 'activity-tracker-cache-v2a';
+const CACHE_NAME = 'activity-tracker-cache-v2b';
 
 // The name of your GitHub repository. This is crucial for creating the correct paths.
 const REPO_NAME = '/activity-tracker-html';
